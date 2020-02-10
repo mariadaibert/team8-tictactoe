0eda0e0e6a26e1972f2664edca02509a896cd77b
Maria Daibert <mariardaibert@gmail.com>
2020-02-10 20:06:33 +0000

    recursão função jogada

--- jogada.py
+++ jogada.py
@@ -1,13 +1,14 @@
 #Captura jogada
+import jogada
+
 def jogar(peca_jogador,matriz):
-    inv=1
-    while(inv==1):
-        print("Jogue, Player"+str(peca_jogador))
-        x=int(input("Linha:"))
-        y=int(input("Coluna:"))
-        if(matriz[x-1][y-1]==100):
-            matriz[x-1][y-1]=peca_jogador
-            inv=0
-        else:
-            print("Jogada inválida.")
+    print("Jogue, Player "+str(peca_jogador))
+    x=int(input("Linha:"))
+    y=int(input("Coluna:"))
+    if(matriz[x-1][y-1]==100):
+        matriz[x-1][y-1]=peca_jogador
+    else:
+        print("Jogada inválida.")
+        jogada.jogar(peca_jogador,matriz)
+    
     return matriz
